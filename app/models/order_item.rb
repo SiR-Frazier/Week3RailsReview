a86cd08216a8b0dad2fdd6815b384583c27c86e6
Bryce Frazier <bx.frazier@gmail.com>
2018-05-04 09:38:01 -0700

    initial commit

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+  validates_presence_of :quantity #necessary?
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   has_many :order_items
+  validates_presence_of :price #necessary?
 end
