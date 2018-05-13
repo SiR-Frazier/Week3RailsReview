15d6423c1c018c5c65fdc333942f5f144ecbf7e0
sir-frazier <bxfrazier@gmail.com>
2018-05-12 18:08:39 -0700

    add password length validations to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
+  # validates :email, presence: true, uniqueness: true
+  validates :password_digest, length: { minimum: 8 }, allow_nil: true
+
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
